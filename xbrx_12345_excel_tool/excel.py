37dc958941f09d254d8a6e39a56c2318b65eb056
lightblue <weilancys@hotmail.com>
2020-11-22 23:58:20 +0800

    added start and end time widgets

--- xbrx_12345_excel_tool/excel.py
+++ xbrx_12345_excel_tool/excel.py
@@ -1,2 +1,9 @@
 import openpyxl
+import datetime
 
+
+def find_rows_by_time(start_datetime, end_datetime):
+    """
+    docstring
+    """
+    pass
--- xbrx_12345_excel_tool/ui.py
+++ xbrx_12345_excel_tool/ui.py
@@ -3,6 +3,7 @@ import tkinter.messagebox
 import tkinter.filedialog
 from tkinter import ttk
 
+
 class xbrx_12345_excel_tool(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -37,12 +38,80 @@ class xbrx_12345_excel_tool(tk.Tk):
 
         # frame set time
         frame_set_time = ttk.LabelFrame(tab_split, text="设置起始时间：")
-        label = ttk.Label(frame_set_time, text="1234")
-        label.pack()
+
+        # start time input widgets
+        label_start = ttk.Label(frame_set_time, text="起：")
+        label_start.grid(row=0, column=0)
+
+        cb_start_year = ttk.Combobox(frame_set_time, width=5)
+        cb_start_year.grid(row=0, column=1)
+        label_start_year = ttk.Label(frame_set_time, text="年")
+        label_start_year.grid(row=0, column=2)
+
+        cb_start_month = ttk.Combobox(frame_set_time, width=3)
+        cb_start_month.grid(row=0, column=3)
+        label_start_month = ttk.Label(frame_set_time, text="月")
+        label_start_month.grid(row=0, column=4)
+
+        cb_start_day = ttk.Combobox(frame_set_time, width=3)
+        cb_start_day.grid(row=0, column=5)
+        label_start_day = ttk.Label(frame_set_time, text="日")
+        label_start_day.grid(row=0, column=6)
+
+        cb_start_hour = ttk.Combobox(frame_set_time, width=3)
+        cb_start_hour.grid(row=0, column=7)
+        label_start_hour = ttk.Label(frame_set_time, text="时")
+        label_start_hour.grid(row=0, column=8)
+
+        cb_start_minute = ttk.Combobox(frame_set_time, width=3)
+        cb_start_minute.grid(row=0, column=9)
+        label_start_minute = ttk.Label(frame_set_time, text="分")
+        label_start_minute.grid(row=0, column=10)
+
+        cb_start_second = ttk.Combobox(frame_set_time, width=3)
+        cb_start_second.grid(row=0, column=11)
+        label_start_second = ttk.Label(frame_set_time, text="秒")
+        label_start_second.grid(row=0, column=12)
+
+
+        # end time input widgets
+        label_end = ttk.Label(frame_set_time, text="止：")
+        label_end.grid(row=1, column=0)
+
+        cb_end_year = ttk.Combobox(frame_set_time, width=5)
+        cb_end_year.grid(row=1, column=1)
+        label_end_year = ttk.Label(frame_set_time, text="年")
+        label_end_year.grid(row=1, column=2)
+
+        cb_end_month = ttk.Combobox(frame_set_time, width=3)
+        cb_end_month.grid(row=1, column=3)
+        label_end_month = ttk.Label(frame_set_time, text="月")
+        label_end_month.grid(row=1, column=4)
+
+        cb_end_day = ttk.Combobox(frame_set_time, width=3)
+        cb_end_day.grid(row=1, column=5)
+        label_end_day = ttk.Label(frame_set_time, text="日")
+        label_end_day.grid(row=1, column=6)
+
+        cb_end_hour = ttk.Combobox(frame_set_time, width=3)
+        cb_end_hour.grid(row=1, column=7)
+        label_end_hour = ttk.Label(frame_set_time, text="时")
+        label_end_hour.grid(row=1, column=8)
+
+        cb_end_minute = ttk.Combobox(frame_set_time, width=3)
+        cb_end_minute.grid(row=1, column=9)
+        label_end_minute = ttk.Label(frame_set_time, text="分")
+        label_end_minute.grid(row=1, column=10)
+
+        cb_end_second = ttk.Combobox(frame_set_time, width=3)
+        cb_end_second.grid(row=1, column=11)
+        label_end_second = ttk.Label(frame_set_time, text="秒")
+        label_end_second.grid(row=1, column=12)
+
         frame_set_time.pack(fill=tk.X)
 
         # button export
-        btn_export = ttk.Button(tab_split, text="导出小白热线模板")
+        btn_export = ttk.Button(tab_split, text="导出热线系统模板")
         btn_export.pack()
 
     
