71937a6736091eb5ec8198c99b9c1de27cd07a99
lbcoder <lbcoder@hotmail.com>
2020-12-21 14:48:37 +0800

    fixed missing dependences in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,9 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     install_requires = [
-        "openpyxl"
+        "openpyxl",
+        "xlrd",
+        "jinja2",
     ],
     classifiers = [
         "Programming Language :: Python :: 3",
